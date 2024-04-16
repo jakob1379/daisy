53f93c6fcd7b8d446c0b34c2aa7131dac60974d2
Silas <silas@di.ku.dk>
2024-04-16 13:20:38 +0200

    Ignore subdirs when summarizing coverage for a directory

--- test/coverage/dai_system_test_coverage_details.sh
+++ test/coverage/dai_system_test_coverage_details.sh
@@ -8,7 +8,7 @@ out_file=${out_dir}/coverage.html
 rm -rf ${out_dir}
 mkdir -p ${out_dir}
 params=("-r" "${src_dir}" "${build_dir}" "--html-details" "${out_file}" "--sort-percentage" "--exclude" "(.+/)ut_.+\.C$")
-params+=("-f" "(.+/)?${dir}/.+\.C$")
+params+=("-f" "(.+/)?${dir}/[^/]+\.C$")
 
 echo "Summarizing test coverage for ${dir}"
 gcovr ${params[*]} && echo "Coverage report in file://$(realpath ${out_file})"
